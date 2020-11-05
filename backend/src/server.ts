6d06784f1f258f9e2cb7a9fc22fe20d4f36db348
estevaowat <estevao.watanabe@gmail.com>
2020-11-04 21:36:13 -0300

    remove node_modules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,30 @@
-import express, {Request, Response} from 'express';
-import http from 'http'
-import socketIO from 'socket.io'
+import express, { Request, Response } from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
 
 const io = socketIO(server).listen(4000);
 
-app.get('/', (req:Request, res:Response) => {
-  return res.json({
-    message: "Hello World"
-  })
-})
+interface Join {
+  name: string;
+  room: string;
+}
 
- io.on("connection", (socket )=> {
-  socket.emit("api", {
+app.get('/', (req: Request, res: Response) => {
+  return res.json({
+    message: 'Hello World',
+  });
+});
+
+io.on('connection', socket => {
+  socket.emit('api', {
     user: 'estevaowat',
-    date: new Date()
-  })
- })
- 
- 
- 
+    date: new Date(),
+  });
+});
+
+io.on('join', ({ name, room }: Join) => {
+  console.log(name, room);
+});
